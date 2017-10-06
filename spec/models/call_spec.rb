1f6f2f41fba3f02079840d977b9db9ab5feab3d0
Christoph Wagner <github@pandawhisperer.net>
2017-10-06 06:35:29 -0700

    First test

--- spec/models/call_spec.rb
+++ spec/models/call_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Call, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :users
+
+  it 'extracts phone numbers on create' do
+    call = Call.create(from_user: users(:earnie), to_user: users(:bert))
+    expect(call.from_number).to match(/\+1\d{10}/)
+    expect(call.to_number).to match(/\+1\d{10}/)
+  end
+
 end
